47e459268aac22c0f72af1fbae24642ef16ec3fc
Leo Knudsen <lk22>
2023-06-30 20:28:23 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify/functions";
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  output: "server",
+  adapter: netlify()
+});
